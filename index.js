ac5ae36ed4bab2018966cce547cd62682f319b63
Vincent <vincecreton@hotmail.fr>
2020-11-01 09:52:57 +0100

    FEATURE: Traque de la souris et affichage du marqueur personnage en fonction de sa position

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function() {
     let canvas = document.getElementById('game'),
-        context = canvas.getContext('2d');
+        ctx = canvas.getContext('2d');
 
     let chrono = document.getElementById('chrono');
     let chronoValue = chrono.querySelector('.chrono-value');
@@ -10,9 +10,14 @@
     let chronoRun = false;
     let timeoutID;
 
+    let playerMarquer = new Image();
+    /* Variable permettant de savoir si le jeu est lancé */
+    let gameRunning = false;
+
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
     window.addEventListener('click', launchGame, false);
+    document.onmousemove = handleMouseMove;
 
     function resizeCanvas() {
         canvas.width = window.innerWidth;
@@ -28,15 +33,20 @@
 
     function drawStuff() {
         // do your drawing stuff here
+        playerMarquer.onload = () => {
+            ctx.drawImage(playerMarquer, 0, 0, 115, 115);
+        }
+        playerMarquer.src = './assets/images/vinc-projet.png';
     }
 
     function launchGame() {
+        gameRunning = true;
         launchChrono();
     }
     function launchChrono() {
         timeoutID = window.setTimeout(increaseChronoValue, 10);
         window.removeEventListener('click', launchGame, false);
-        window.addEventListener('click', () => {window.clearTimeout(timeoutID)}, false)
+        window.addEventListener('click', () => {window.clearTimeout(timeoutID); gameRunning = false;}, false)
     }
     function increaseChronoValue() {
         chronoValueInt += 1;
@@ -49,5 +59,34 @@
             chronoValueSeconds.innerHTML = chronoValueSecondsInt.toString();
         }
         timeoutID = window.setTimeout(increaseChronoValue, 10);
+    }
+
+    /**
+     * La fonction suivante va nous permettre de traquer le marqueur du joeur et afficher le marqueur en fonction de la nouvelle position
+     * @param event
+     */
+
+    function handleMouseMove(event) {
+        let eventDoc, doc, body;
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+                (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+                (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+                (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+                (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+
+        if(gameRunning) {
+            ctx.drawImage(playerMarquer, event.pageX, event.pageY, 115, 115);
+        }
     }
 })();
---
+++ src/JS/handleMouseMove.js
@@ -0,0 +1,25 @@
+function handleMouseMove(event) {
+    let eventDoc, doc, body;
+
+    // If pageX/Y aren't available and clientX/Y are,
+    // calculate pageX/Y - logic taken from jQuery.
+    // (This is to support old IE)
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+            (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+            (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    if(gameRunning) {
+        ctx.drawImage(playerMarquer, event.pageX, event.pageY, 115, 115);
+    }
+}
+
+export default handleMouseMove;
